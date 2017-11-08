b333c0b66c743cf66fe5cac044a47d127254f5b7
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-08 23:17:39 +0100

    added some layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'restaurants#index'
 
   resources :restaurants, except: [:edit, :update, :destroy] do
-      resources :reviews, except: [:edit, :update, :destroy]
+    resources :reviews, except: [:edit, :update, :destroy]
   end
 end
